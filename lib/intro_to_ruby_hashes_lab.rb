668fabf00ef328e3cb5d249c52db4960d97db2f2
Ariba Khan <akhan41@binghamton.edu>
2020-08-05 17:58:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  { fav_pizza : "jalepeno"}
+  { fav_pizza: "jalepeno"}
 end
 
 def pioneer
-{ name : "Grace Hopper"}
+{ name: "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number}
 end
